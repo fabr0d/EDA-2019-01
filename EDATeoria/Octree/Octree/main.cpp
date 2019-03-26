c1cbf08a8b609274f207cf3a43669e498bda590c
npc <fabrizio.flores@ucsp.edu.pe>
2019-03-25 21:17:14 -0500

    code

---
+++ EDATeoria/Octree/Octree/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+class Octree
+{
+public:
+	Octree();
+	~Octree();
+	void find(){}
+	void insert(){}
+	void delet(){}
+	Octree *r1, *r2, *r3, *r4, *r5, *r6, *r7, *r8;
+
+private:
+
+};
+
+Octree::Octree()
+{
+}
+
+Octree::~Octree()
+{
+}
+
+
+int main() {
+	cout << "Aiuda D: !" << endl;
+	getchar();
+}
