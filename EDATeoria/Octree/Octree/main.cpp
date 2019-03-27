4f0e43788614aeaeb0975d0d1455ee91d8c512f7
npc <fabrodaqp@gmail.com>
2019-03-27 16:07:19 -0500

    code

--- EDATeoria/Octree/Octree/main.cpp
+++ EDATeoria/Octree/Octree/main.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
+
+struct Point
+{
+	double x;
+	double y;
+	double z;
+};
+
+
+//Nota: En el Octree los puntos que delimiten las dimensiones el primero debe ser 
+bool ItsIn(vector<Point> Ranges, Point NPoint) 
+{
+	if (NPoint.x > Ranges[0].x and NPoint.x < Ranges[1].x &&
+		NPoint.y > Ranges[0].y and NPoint.y < Ranges[1].y &&
+		NPoint.z > Ranges[0].z and NPoint.z < Ranges[1].z )
+	{
+		return true;
+	}
+	return false;
+}
 
 class Octree
 {
