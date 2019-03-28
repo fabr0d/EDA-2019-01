108a0e41ac27ed8c863c5f3e8ba5e65745d0afdf
npc <fabrodaqp@gmail.com>
2019-03-28 17:17:06 -0500

    code

--- EDATeoria/Octree/Octree/main.cpp
+++ EDATeoria/Octree/Octree/main.cpp
@@ -9,41 +9,50 @@ struct Point
 	double z;
 };
 
-
-//Nota: En el Octree los puntos que delimiten las dimensiones el primero debe ser 
-bool ItsIn(vector<Point> Ranges, Point NPoint) 
+bool ItsIn(vector<Point> Ranges, Point NPoint)
 {
 	if (NPoint.x > Ranges[0].x and NPoint.x < Ranges[1].x &&
 		NPoint.y > Ranges[0].y and NPoint.y < Ranges[1].y &&
-		NPoint.z > Ranges[0].z and NPoint.z < Ranges[1].z )
+		NPoint.z > Ranges[0].z and NPoint.z < Ranges[1].z)
 	{
 		return true;
 	}
 	return false;
 }
 
-class Octree
+class Cube
 {
 public:
-	Octree();
-	~Octree();
-	void find(){}
-	void insert(){}
-	void delet(){}
-	Octree *r1, *r2, *r3, *r4, *r5, *r6, *r7, *r8;
+	vector<Point> Limits;
+	vector<Point> Elements;
+	bool Status;
+	vector<Cube> Sons;
+	Cube(Point P1, Point P2) {
+		Limits.push_back(P1);
+		Limits.push_back(P2);
+	}
+	~Cube();
 
 private:
 
 };
 
-Octree::Octree()
-{
+
+//True = partido, false = !partido
+
+//Nota: En el Octree los puntos que delimiten las dimensiones el primero debe ser 
+
+void find(Cube Octree, Point Pnt) {
+
 }
 
-Octree::~Octree()
-{
+void insert(Cube Octree, Point Pnt) {
+
 }
 
+void erase(Cube Octree, Point Pnt) {
+
+}
 
 int main() {
 	cout << "Aiuda D: !" << endl;
