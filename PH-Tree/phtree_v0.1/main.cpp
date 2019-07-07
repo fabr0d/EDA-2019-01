05a3858bc1a2029b03ecabda286e7a4b989c26cd
npc <fabrizio.flores@ucsp.edu.pe>
2019-07-07 13:24:44 -0500

    phtree final_vsn :D
    to do: change insert form

--- PH-Tree/phtree_v0.1/main.cpp
+++ PH-Tree/phtree_v0.1/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <chrono>
 #include <vector>
 #include <bitset>
 #include <map>
 #include <utility> 
 #include <fstream>
 #include <algorithm>
+#include <time.h>
 using namespace std;
 int N_bits = 4;
 
@@ -83,7 +85,7 @@ class LHC
 {
 public:
 
-	bool Isleaf = false;
+	//bool Isleaf = false;
 	vector<pair<bitset<64>, LHC*>> DatosEHijos;
 	vector<bool> IsVisited;
 
@@ -97,14 +99,14 @@ public:
 
 		while (cont < 64)
 		{
-			cout << "p: " << p << endl;
+			//cout << "p: " << p << endl;
 			seEncontro = false;
 			if (p->DatosEHijos.size() == 0)
 			{
-				cout << "esta vacio el hypercubo actual no tiene prefijos" << endl;
+				//cout << "esta vacio el hypercubo actual no tiene prefijos" << endl;
 				pair <bitset<64>, LHC*> temporal;
 				temporal.first = KPoint.BitsEnPosXDeCadaDimDeUnDato(cont + 1);
-				cout << "uso de la funcion BitsEnPosXDeCadaDimDeUnDato en el nivel " << cont << " : " << temporal.first << endl;
+				//cout << "uso de la funcion BitsEnPosXDeCadaDimDeUnDato en el nivel " << cont << " : " << temporal.first << endl;
 
 				temporal.second = new LHC;
 				//cout << "temporal.second: " << temporal.second << endl;
@@ -112,46 +114,48 @@ public:
 				p->IsVisited.push_back(false);
 				p = temporal.second;
 				cont++;
-				cout << "----------------------------------------------------" << endl;
+				//cout << "----------------------------------------------------" << endl;
 			}
 			else
 			{
-				cout << "en el hypercubo hay datos" << endl;
+				//cout << "en el hypercubo hay datos" << endl;
 				Bitstemp = KPoint.BitsEnPosXDeCadaDimDeUnDato(cont + 1);
 				for (int i = 0; i < p->DatosEHijos.size(); i++)
 				{
-					cout << p->DatosEHijos[i].first << " ==?" << Bitstemp << endl;
+					//cout << p->DatosEHijos[i].first << " ==?" << Bitstemp << endl;
 					if (p->DatosEHijos[i].first == Bitstemp)
 					{
-						cout << "encontre el prefijo en el hypercubo asi que solo bajo" << endl;
+						//cout << "encontre el prefijo en el hypercubo asi que solo bajo" << endl;
 						//cout << "p antes de: " << p << " - p->DatosEHijos[i].second: " << p->DatosEHijos[i].second << endl;
 						p = p->DatosEHijos[i].second;
 						cont++;
 						seEncontro = true;
-						cout << "----------------------------------------------------" << endl;
+						break;
+						//cout << "----------------------------------------------------" << endl;
 					}
 				}
 				if (seEncontro == false)
 				{
-					cout << "hay datos en el hypercubo pero no esta el prefijo" << endl;
+					//cout << "hay datos en el hypercubo pero no esta el prefijo" << endl;
 
 					pair <bitset<64>, LHC*> temporal;
 					temporal.first = KPoint.BitsEnPosXDeCadaDimDeUnDato(cont + 1);
 					temporal.second = new LHC;
 					p->DatosEHijos.push_back(temporal);
-					cout << "inserto el prefijo que me falta : " << temporal.first << endl;
+					//cout << "inserto el prefijo que me falta : " << temporal.first << endl;
 					p->IsVisited.push_back(false);
 					p = temporal.second;
 					cont++;
-					cout << "----------------------------------------------------" << endl;
+					//cout << "----------------------------------------------------" << endl;
 				}
 			}
 		}
-		cout << "************************************FIN INSERT************************************" << endl;
+		//cout << "************************************FIN INSERT************************************" << endl;
 	}
 
 	bool search(Hyperdata KPoint)
 	{
+		//cout << "*********************inicio serach*********************" << endl;
 		LHC* p = this;
 		bitset<64> Bitstemp;
 		int cont = 0;
@@ -160,7 +164,7 @@ public:
 
 		while (cont < 64)
 		{
-			//cout << "p: " << p << endl;
+			//cout << "lvl: " << cont << endl;
 			seEncontro = false;
 			if (p->DatosEHijos.size() == 0)
 			{
@@ -171,9 +175,10 @@ public:
 			{
 				//cout << "en el hypercubo hay datos" << endl;
 				Bitstemp = KPoint.BitsEnPosXDeCadaDimDeUnDato(cont + 1);
-				for (int i = 0; i < p->DatosEHijos.size(); i++)
+
+				int i = 0;
+				/*while (i < p->DatosEHijos.size())
 				{
-					cout << p->DatosEHijos[i].first << " ==?" << Bitstemp << endl;
 					if (p->DatosEHijos[i].first == Bitstemp)
 					{
 						cout << "encontre el prefijo en el hypercubo asi que solo bajo" << endl;
@@ -182,20 +187,37 @@ public:
 						cont++;
 						seEncontro = true;
 						contadorSearch++;
+
+						//cout << "----------------------------------------------------" << endl;
+					}
+					i++;
+				}*/
+
+				for (int i = 0; i < p->DatosEHijos.size(); i++)
+				{
+					//cout << p->DatosEHijos[i].first << " ==?" << Bitstemp << endl;
+					if (p->DatosEHijos[i].first == Bitstemp)
+					{
+						//cout << "encontre el prefijo en el hypercubo asi que solo bajo" << endl;
+						//cout << "p antes de: " << p << " - p->DatosEHijos[i].second: " << p->DatosEHijos[i].second << endl;
+						p = p->DatosEHijos[i].second;
+						cont++;
+						seEncontro = true;
+						contadorSearch++;
+						break;
 						//cout << "----------------------------------------------------" << endl;
 					}
 				}
 				if (seEncontro == false)
 				{
 					//cout << "hay datos en el hypercubo pero no esta el prefijo" << endl;
-
 					return false;
 				}
 			}
 		}
 		if (contadorSearch == 64)
 		{
-			cout << "************************************FIN SEARCH************************************" << endl;
+			//cout << "******************FIN SEARCH*****************" << endl;
 			return true;
 		}
 	}
@@ -220,20 +242,24 @@ public:
 
 		while (cont < 64)
 		{
-			cout << "p: " << p << endl;
+			//cout << "p: " << p << endl;
 			seEncontro = false;
-			if (p->DatosEHijos.size() == 0) { cout << "no esta el valor :s" << endl; }
+			if (p->DatosEHijos.size() == 0) 
+			{ 
+				//cout << "no esta el valor, nivel sin datos" << endl; 
+				return knn; 
+			}
 			else
 			{
 				//cout << "en el hypercubo hay datos" << endl;
 				Bitstemp = KPoint.BitsEnPosXDeCadaDimDeUnDato(cont + 1);
 				for (int i = 0; i < p->DatosEHijos.size(); i++)
 				{
-					cout << p->DatosEHijos[i].first << " ==?" << Bitstemp << endl;
+					//cout << p->DatosEHijos[i].first << " ==?" << Bitstemp << endl;
 					if (p->DatosEHijos[i].first == Bitstemp)
 					{
 						toRec.push_back(Bitstemp);
-						cout << "encontre el prefijo en el hypercubo asi que solo bajo" << endl;
+						//cout << "encontre el prefijo en el hypercubo asi que solo bajo" << endl;
 
 						pair<LHC*, int> PathTemp;
 						PathTemp.first = p;
@@ -241,35 +267,41 @@ public:
 						path.push_back(PathTemp);
 						p->IsVisited[i] = true;
 
-						cout << "p antes de: " << p << ", ahora tendra la direccion de:  " << p->DatosEHijos[i].second << endl;
+						//cout << "p antes de: " << p << ", ahora tendra la direccion de:  " << p->DatosEHijos[i].second << endl;
 						p = p->DatosEHijos[i].second;
 						cont++;
 						seEncontro = true;
 						contadorSearch++;
+						break;
 						//cout << "----------------------------------------------------" << endl;
 					}
 				}
-				if (seEncontro == false) { cout << "no esta el valor :s" << endl; }
+				if (seEncontro == false) 
+				{ 
+					//cout << "no esta el valor , habian datos en el hypercubo pero no el prefijo que queria" << endl; 
+					return knn;
+				}
 			}
 		}
 		if (contadorSearch == 64)
 		{
-			cout << "se encontro el dato" << endl;
+			//cout << "se encontro el dato" << endl;
+			/*
 			for (int i = 0; i < path.size(); i++)
 			{
 				cout << "su respectivo direccion: " << path[i].first << " , su pos en el vector DatosEHijos: " << path[i].second << endl;
-			}
+			}*/
 		}
 		//
 		//paso a preguntar en el ultimo nivel si tiene mas de el valor encontrado
 		p = path[path.size() - 1].first;
-		cout << "p: " << p << endl;
+		//cout << "p: " << p << endl;
 		//cout << "p->DatosEHijos.size(): " << p->DatosEHijos.size() << endl;
 
 
 		if (p->DatosEHijos.size() > 1)
 		{
-			printBoolVec(p->IsVisited);
+			//printBoolVec(p->IsVisited);
 			for (int i = 0; i < p->DatosEHijos.size(); i++)
 			{
 				if (p->IsVisited[i] == false)
@@ -277,39 +309,39 @@ public:
 					toRec[63] = p->DatosEHijos[i].first;
 					vector<bitset<64>> recons = reconstructHD(toRec);
 					knn.push_back(recons);
-					cout << "encontre un knn !!!!!!! : " << knn.size() << endl;
+					//cout << "encontre un knn !!!!!!! : " << knn.size() << endl;
 					p->IsVisited[i] = true;
 				}
 			}
 
-			cout << "los vecinos mas cercanos encontrados en el mismo ultimo hypercubo" << endl;
-			for (int a = 0; a < knn.size(); a++)
+			//cout << "los vecinos mas cercanos encontrados en el mismo ultimo hypercubo" << endl;
+			/*for (int a = 0; a < knn.size(); a++)
 			{
 				printBitVec(knn[a]);
-			}
+			}*/
 		}
 		if (knn.size() < k)
 		{
-			cout << "------me faltan knns :s ---------" << endl;
+			//cout << "------me faltan knns :s ---------" << endl;
 			int altura = 64 - 1; // numero de bits -1
-			cout << "p que apunta al hijo donde encontre los primeros knn: " << p << endl;
+			//cout << "p que apunta al hijo donde encontre los primeros knn: " << p << endl;
 			path.pop_back();
-			cout << "borro el la ultima direccion porque es donde encontre los knn rapidos" << endl;
-			cout << "imprimo el path que ma queda:" << endl;
-			for (int i = 0; i < path.size(); i++)
+			//cout << "borro el la ultima direccion porque es donde encontre los knn rapidos" << endl;
+			//cout << "imprimo el path que ma queda:" << endl;
+			/*for (int i = 0; i < path.size(); i++)
 			{
 				cout << "su respectivo direccion: " << path[i].first << " , su pos en el vector DatosEHijos: " << path[i].second << endl;
-			}
-			cout << "p ya apunta a en penultino hypercubo" << endl;
+			}*/
+			//cout << "p ya apunta a en penultino hypercubo" << endl;
 			p = path[path.size() - 1].first;
-			cout << "p: " << p << endl;
+			//cout << "p: " << p << endl;
 			printBoolVec(p->IsVisited);
 
 			while (knn.size() < k)
 			{
-				cout << "altura : " << altura << endl;
-				cout << "paso para verificar si en el hypercubo que estoy ya todos fueron visitados" << endl;
-				printBoolVec(p->IsVisited);
+				//cout << "altura : " << altura << endl;
+				//cout << "paso para verificar si en el hypercubo que estoy ya todos fueron visitados" << endl;
+				//printBoolVec(p->IsVisited);
 				int contabuls = 0;
 				for (int i = 0; i < p->DatosEHijos.size(); i++)
 				{
@@ -322,37 +354,37 @@ public:
 
 				if (contabuls == p->DatosEHijos.size()) //si todos fueron visitados subo un nivel en el arbol
 				{
-					cout << "si todos fueron visitados subo un nivel en el arbol" << endl;
-					cout << "p actual: " << p << endl;
+					//cout << "si todos fueron visitados subo un nivel en el arbol" << endl;
+					//cout << "p actual: " << p << endl;
 					path.pop_back();
-					for (int i = 0; i < path.size(); i++)
+					/*for (int i = 0; i < path.size(); i++)
 					{
 						cout << "su respectivo direccion: " << path[i].first << " , su pos en el vector DatosEHijos: " << path[i].second << endl;
-					}
+					}*/
 					p = path[path.size() - 1].first;
-					cout << "nuevo p: " << p << endl;
+					//cout << "nuevo p: " << p << endl;
 					altura--;
 				}
 				else // si no todos fueron visitados
 				{
-					cout << "en el nivel donde estoy aun hay por visitar: " << endl;
-					printBoolVec(p->IsVisited);
+					//cout << "en el nivel donde estoy aun hay por visitar: " << endl;
+					//printBoolVec(p->IsVisited);
 
 					bool kappa = false;
-					cout << "paso a verificar cuales no estan visitados de un total de " << DatosEHijos.size() << "elementos " << endl;
+					//cout << "paso a verificar cuales no estan visitados de un total de " << DatosEHijos.size() << "elementos " << endl;
 					for (int i = 0; i < p->DatosEHijos.size(); i++)
 					{
 						if (p->IsVisited[i] == false)
 						{
-							cout << "si no esta visitado" << endl;
+							//cout << "si no esta visitado" << endl;
 							toRec[altura - 1] = p->DatosEHijos[i].first; //modifico el prefijo
 							if (altura == 64) //estoy en las hojas // numero de bits
 							{
-								cout << "si estoy en el ultimo nivel del arbol" << endl;
+								//cout << "si estoy en el ultimo nivel del arbol" << endl;
 								kappa = true;
 								vector<bitset<64>> recons = reconstructHD(toRec);
 								knn.push_back(recons);
-								cout << "pucheo un knn nuevo :D" << endl;
+								//cout << "pucheo un knn nuevo :D" << endl;
 								p->IsVisited[i] = true;
 								if (knn.size() >= k)
 								{
@@ -361,9 +393,9 @@ public:
 							}
 							else //bajo
 							{
-								cout << "tengo que bajar un nivel porque no estoy en una hoja, marco como visitado y bajo" << endl;
+								//cout << "tengo que bajar un nivel porque no estoy en una hoja, marco como visitado y bajo" << endl;
 								p->IsVisited[i] = true;
-								cout << "p antes de bajar: " << p << ", ahora tendra la direccion de un nodo mas abajo:  " << p->DatosEHijos[i].second << endl;
+								//cout << "p antes de bajar: " << p << ", ahora tendra la direccion de un nodo mas abajo:  " << p->DatosEHijos[i].second << endl;
 								p = p->DatosEHijos[i].second;
 
 								pair<LHC*, int> PathTemp2;
@@ -405,8 +437,13 @@ int main()
 	filename = "C:\\Users\\Fabrizio\\source\\repos\\tests\\DataSetRand.txt";
 	file.open(filename.c_str());
 
-	for (int i = 0; i < 100000; i++)
+	
+	//data set de year prediccion de 90 dimensiones y cada num * 10 000 para evitar decimales - 515345 puntos k dim
+	vector<Hyperdata> NDimDatas;
+
+	for (int i = 0; i < 999999999; i++)
 	{
+		//cout << "i: " << i << endl;
 		vector<bitset<64>> tosend;
 		vector<int> nums;
 
@@ -420,15 +457,107 @@ int main()
 			tosend.push_back(temp);
 
 			cont++;
+
 		}
 		cont = 0;
 		Hyperdata toinsert(tosend);
-		//printIntVec(nums);
-		//printBitVec(toinsert.datas);
-		//phtree.insert(toinsert);
-	}
-	
 
+		//printIntVec(nums);
+		NDimDatas.push_back(toinsert);
+		
+		//printBitVec(toinsert.datas);
+		
+	}
+	file.close();
+	cout << "numero de datos: " << NDimDatas.size() << endl;
+
+	auto start = chrono::steady_clock::now();
+	for (size_t i = 0; i < NDimDatas.size(); i++)
+	{
+		phtree.insert(NDimDatas[i]);
+		//cout << "se inserto id " <<i<< endl;
+	}
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+	cout << chrono::duration <double, milli>(diff).count() << " ms" << endl;
+	cout << "fin del insert" << endl;
+
+
+	cout << "ahora el find  test" << endl;///////////////////////////////////////////////
+
+	int contB = 0;
+
+	fstream fileB;
+	int numB;
+	string filenameB;
+	filenameB = "C:\\Users\\Fabrizio\\source\\repos\\tests\\DataSetRand.txt";
+	fileB.open(filenameB.c_str());
+
+	vector<Hyperdata> NDimDatasB;
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "i: " << i << endl;
+		vector<bitset<64>> tosend;
+		vector<int> nums;
+
+		while (cont < 64)
+		{
+			fileB >> num;
+
+			nums.push_back(num);
+
+			bitset<64> temp(num);
+			tosend.push_back(temp);
+
+			cont++;
+
+		}
+		cont = 0;
+		Hyperdata toinsert(tosend);
+
+		printIntVec(nums);
+		NDimDatasB.push_back(toinsert);
+		//printBitVec(toinsert.datas);
+
+	}
+	fileB.close();
+
+	cout << "numero de datos: " << NDimDatasB.size() << endl;
+
+	auto startB = chrono::steady_clock::now();
+	for (size_t i = 0; i < NDimDatasB.size(); i++)
+	{
+		cout << "phtree.search(NDimDatasB[i]): " << phtree.search(NDimDatasB[i]) << endl;
+		cout << "se busco id " << i << endl;
+	}
+	auto endB = chrono::steady_clock::now();
+	auto diffB = endB - startB;
+	cout << chrono::duration <double, milli>(diffB).count() << " ms" << endl;
+	cout << "fin del insert" << endl;
+
+	cout << " FIN DEL SEARCH TEST" << endl;/////////////////////////////////////////////
+
+	cout << "TEST DE LOS KNN--------------------" << endl;
+
+	//rreuso NDimDatasB de los 4 datos que busque
+
+	vector<vector<bitset<64>>> knns;
+	auto startC = chrono::steady_clock::now();
+
+	knns = phtree.KNNQuery(100, NDimDatasB[0]);
+	cout << "numero de knn encontrados: " << knns.size() << endl;
+
+	auto endC = chrono::steady_clock::now();
+	auto diffC = endC - startC;
+	cout << chrono::duration <double, milli>(diffC).count() << " ms" << endl;
+	
+	/*for (int i = 0; i < knns.size(); i++)
+	{
+		printBitVec(knns[i]);
+	}*/
+	
+	cout << "fin del knn QUERY" << endl;
 	/*
 	LHC phtree;
 
